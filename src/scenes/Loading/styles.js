30091dcc685f3781dab4fe0acd7235e8b72da834
asafnir <asafnirs@gmail.com>
2018-11-01 16:25:13 -0700

    Add style for loading page

---
+++ src/scenes/Loading/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#121212",
+    alignItems: 'center',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  },
+});
+
+
+export default styles;
